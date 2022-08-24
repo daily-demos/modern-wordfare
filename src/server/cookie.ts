20c6d89a3ca8119f2686958a80414855320b0011
Liza Shulyayeva <65890040+lazeratops@users.noreply.github.com>
2022-08-24 18:19:09 +0200

    add some explanatory comments

--- src/server/cookie.ts
+++ src/server/cookie.ts
@@ -10,10 +10,15 @@ export function setupCookieParser(app: Application) {
   app.use(cookieParser(DAILY_API_KEY));
 }
 
+// getGameHostCookieName() returns the intended name of the
+// game host cookie.
 export function getGameHostCookieName(gameID: string): string {
   return `gh-${gameID}`;
 }
 
+// getGameHostCookie() takes all available cookies and a game ID, and returns
+// the value of the game host cookie, if found. The value will be the creation
+// time of the cookie.
 export function getGameHostCookie(cookies: string, gameID: string): number {
   const parts = cookies.split(`${getGameHostCookieName(gameID)}=`);
   if (parts.length !== 2) {
@@ -21,14 +26,26 @@ export function getGameHostCookie(cookies: string, gameID: string): number {
   }
   const val = parts.pop().split("; ").shift();
   const decodedCookie = decodeURIComponent(val);
-  const parsed = cookieParser.signedCookie(decodedCookie, DAILY_API_KEY);
-  if (!decodedCookie || decodedCookie === parsed) return -1;
 
+  // We expect the cookie to be signed. If cookie parser returns
+  // false or an identical value to what was passed in, something
+  // went wrong and we don't have a valid game host cookie.
+  const parsed = cookieParser.signedCookie(decodedCookie, DAILY_API_KEY);
+  if (!parsed || decodedCookie === parsed) return -1;
+
+  // Get numeric representation of cookie value.
+  // If the result is not a number, something went wrong,
+  // meaning we do not have a valid game host cookie.
   const createdAt = +parsed;
-  if (typeof createdAt !== "number") return -1;
+  if (Number.isNaN(createdAt)) return -1;
   return createdAt;
 }
 
+// isGameHostValueValid() takes a cookie creation time
+// and a game creation time, and verifies that the game host
+// cookie was created at maximum 10 seconds after the game.
+// (In reality the creation diff should be much shorter, we're
+// just being extra permissive here).
 export function isGameHostValueValid(
   cookieCreatedAt: number,
   gameCreatedAt: number
@@ -40,6 +57,9 @@ export function isGameHostValueValid(
   return cookieCreatedAt >= earliest && cookieCreatedAt <= latest;
 }
 
+// isGameHostFromSignedCookie() takes an object of Cookies,
+// a game ID, and a game creation time. It retrieves the game
+// host cookie (if any) and validates its value.
 export function isGameHostFromSignedCookies(
   cookies: Cookies,
   gameID: string,
