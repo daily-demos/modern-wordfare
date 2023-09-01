1a0f88351515da21e3105cf7d7d427ba06d98abd
Liza <65890040+lazeratops@users.noreply.github.com>
2023-09-01 16:13:22 +0200

    Audio level indicator feature (#26)
    * Run npm update and linter
    
    * Specify send settings
    
    * Lint fix, add .idea to gitignore
    
    * Audio level demo
    
    * comment update
    
    * comment update
    
    * Update opacity instead of box-shadow
    
    * Limit opacity to two decimal places
    
    * Update box shadow to 6px
    
    * Do not recalculate opacity if audio level is 0 - just set it to 0.

--- src/client/game/board.ts
+++ src/client/game/board.ts
@@ -471,6 +471,10 @@ export class Board {
     }
     participantTile.appendChild(video);
 
+    const audioIndicator = document.createElement("div");
+    audioIndicator.className = "audio-indicator";
+    participantTile.appendChild(audioIndicator);
+
     const nameTag = document.createElement("div");
     nameTag.className = "name";
     nameTag.innerText = name;
@@ -536,6 +540,32 @@ function getTile(participantID: string): HTMLDivElement {
     document.getElementById(participantTileID)
   );
   return participantTile;
+}
+
+export function updateAudioLevel(participantID: string, audioLevel: number) {
+  const participantTile = getTile(participantID);
+  if (!participantTile) {
+    showGameError(
+      new ErrGeneric(`tile for participant ID ${participantID} does not exist`),
+    );
+    return;
+  }
+
+  let opacity = 0;
+  // Audio level will be 0 when player is muted.
+  if (audioLevel !== 0) {
+    // Set opacity to visually reasonable value based on audio level
+    opacity = audioLevel / 0.08;
+    // Clamp opacity to always be between 0 and 1
+    opacity = Math.min(Math.max(opacity, 0), 1);
+    opacity = +opacity.toFixed(2);
+  }
+  const audioIndicator = <HTMLDivElement>(
+    participantTile.getElementsByClassName("audio-indicator")[0]
+  );
+
+  // Update the inner glow shown within the participant tile.
+  audioIndicator.style.setProperty("opacity", `${opacity}`);
 }
 
 // updateMedia() updates the video and audio tracks for
